d31bc3dc70044aa1df8fe2fd995b55dadd4d5447
ROHIT KUMAR RANA <48640973+rohitkumarrana67@users.noreply.github.com>
2020-04-25 14:04:49 +0530

    removed unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("hello world");
-
 const forecastForm = document.querySelector("form");
 const search = document.querySelector("input");
 const message1 = document.querySelector("#errorMessage");
